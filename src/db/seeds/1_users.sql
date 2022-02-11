5eac603882f13b8641b98554cef7bd5e4fc62d96
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-11 17:05:14 -0500

    add users 5-8

--- src/db/seeds/1_users.sql
+++ src/db/seeds/1_users.sql
@@ -1,3 +1,17 @@
 INSERT INTO users (first_name, last_name, user_name, phone_number, email, password, contact_info, user_image, bio, location, gender)
 
-VALUES ('Mariah', 'Carey', 'MCarey', '1020304050', 'mcarey@gmail.com', 'all1want4christmas', '@mariahCarey', '../public/images/mariah_carey_pfp.jpeg', 'this is a bio', 'Beverly Hills', 'Female'), ('Eddie', 'Murphy', 'Eddie_Murphy', '2003004001', 'eddie@emurphy.com', 'password1', '@eddymurphy', '../public/images/eddie_murphy_pfp.jpg', 'this is a bio', 'Hollywood', 'Male'), ('Serena', 'Williams', 'tennis_pro', '2022202111', 's.williams@gmail.com', 'passw0rd2', '@serenawilliams', '../public/images/serena_williams_pfp.jpeg', 'this is a bio', 'NYC', 'Female'), ('Robert', 'Downey Jr', 'RDJ', '1234554321', 'robertDJ@yahoo.com', '1ronm@n', '@robertdowneyjr', '../public/images/Robert_Downey_Jr_pfp.jpg', 'this is a bio', 'Wonderland', 'Male'), 
+VALUES ('Mariah', 'Carey', 'MCarey', '1020304050', 'mcarey@gmail.com', 'all1want4christmas', '@mariahCarey', '../public/images/mariah_carey_pfp.jpeg', 'this is a bio', 'Beverly Hills', 'Female'), 
+
+('Eddie', 'Murphy', 'Eddie_Murphy', '2003004001', 'eddie@emurphy.com', 'password1', '@eddymurphy', '../public/images/eddie_murphy_pfp.jpg', 'this is a bio', 'Hollywood', 'Male'), 
+
+('Serena', 'Williams', 'tennis_pro', '2022202111', 's.williams@gmail.com', 'passw0rd2', '@serenawilliams', '../public/images/serena_williams_pfp.jpeg', 'this is a bio', 'NYC', 'Female'),
+
+('Robert', 'Downey Jr', 'RDJ', '1234554321', 'robertDJ@yahoo.com', '1ronm@n', '@robertdowneyjr', '../public/images/Robert_Downey_Jr_pfp.jpg', 'this is a bio', 'Wonderland', 'Male'), 
+
+('Jim', 'Carrey', 'Jim_Carrey', '1235678921', 'jim_carrey@wise.com', 'oneForall', '@jim_carrey', 'userimage', 'Looking for a roommate temporarily while I visit LA, need someone who can watch over my dogs while I am away. Love to paint', 'Los Angeles', 'Male'), 
+
+('Jeff', 'Goldblum', 'Jeff_Goldblum', '9098621232', 'jeff_goldblum@jurrassic.com', 'Dinosaurs', '@jeff_goldblum', 'userimage', 'My house got destroyed by a T-Rex during Jurrassic World - dominion. Looking for a roommate who can handle being chased by dinosaurs and safeguard my belongings while I save the world', 'San Francisco', 'Male'),
+
+('Blake', 'Lively', 'blake_lively', '3478901022', 'blake_lively@reynolds.com', 'Dinosaurs', '@jeff_goldblum', 'userimage', 'Looking to find a temporary roommate with kids who can hang out with my kids while I stay in chicago.', 'Chicago', 'Female'),
+
+('Nina', 'Dobrev', 'nina_dobrev', '4564443877', 'nina_dobrev@vamp.com', 'ninaClassic', '@ninaDobrev', 'userimage', 'Looking for a roommate who is participating in this year’s olympics', 'British Columbia', 'Female'),
