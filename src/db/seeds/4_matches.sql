2bcc4b06246e996afce1e73a3a6248abbd3d2b53
Safia Abdale <safia.abdale@gmail.com>
2022-02-12 10:46:07 -0500

    Added likes seeds and populated matches table

--- src/db/seeds/4_matches.sql
+++ src/db/seeds/4_matches.sql
@@ -1,1 +1,43 @@
 -- put table of expected matches here in comments for reference
+
+/*
+ 
+all True
+M:T, F:T, O:T, P:T
+  1 - Mariah Carey
+  12 - Olivia Wilde
+
+one False
+M:F, F:T, O:T, P:T
+  3 - Serena Williams
+  7 - Blake Lively
+  8 - Emma Watson
+M:T, F:F, O:T, P:T
+  4 - Robert Downey Jr
+  5 - Jim Carey
+M:T, F:T,  O:F, P:T
+  11 - Jensen Ackles
+M:T, F:T, O:T, P:F
+  2 - Eddie Murphy
+
+two False
+M:T, F:T, O:F, P:F
+M:T, F:F, O:T, P:F
+  6 - Jeff Goldblum
+M:F, F:T, O:T, P:F
+  8 - Nina Dobrev
+M:T, F:F, O:F, P:T
+M:F, F:T, O:F, P:T
+M:F, F:F, O:T, P:T
+
+three False
+M:T, F:F, O:F, P:F
+  9 - Tom Cruise
+M:F, F:T, O:F, P:F
+M:F, F:F, O:T, P:F
+M:F, F:F, O:F, P:T
+
+all False
+M:F, F:F, O:F, P:F
+ 
+*/
