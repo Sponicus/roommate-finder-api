1d1b546bfcb1361e3866439c27f4e7bba53bfa48
Andrew <andrew.spon@gmail.com>
2022-02-15 17:42:35 -0500

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -31,6 +32,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
