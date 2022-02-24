bf0aa6ffb687a9d469c964a9e5b659cc3f0defa3
SaranyaSagi <saranya108sagi@gmail.com>
2022-02-23 21:05:20 -0500

    update images and handles

--- src/db/seeds/1_users.sql
+++ src/db/seeds/1_users.sql
@@ -4,24 +4,24 @@ VALUES ('Mariah', 'Carey', 'MCarey', '1020304050', 'mcarey@gmail.com', 'all1want
 
 ('Eddie', 'Murphy', 'Eddie_Murphy', '2003004001', 'eddie@emurphy.com', 'password1', '@eddymurphy', 'https://i.imgur.com/lOiqUWX.gif', 'Looking for that roommate who is fun, considerate and has a sense of humor? So am I!', 'Hollywood', TRUE), 
 
-('Serena', 'Williams', 'tennis_pro', '2022202111', 's.williams@gmail.com', 'passw0rd2', '@serenawilliams', 'https://i.imgur.com/22N92ON.jpeg', 'I live an active  and healthy lifestyle. I play compeditive sports and am seeking a roommate who is responsible and can apprechiate striving for excellence', 'NYC', FALSE),
+('Serena', 'Williams', 'tennis_pro', '2022202111', 's.williams@gmail.com', 'passw0rd2', '@serenawilliams', 'https://i.imgur.com/22N92ON.jpeg', 'I live an active  and healthy lifestyle. I play competitive sports and am seeking a roommate who is responsible and can appreciate striving for excellence', 'NYC', FALSE),
 
 ('Robert', 'Downey Jr', 'RDJ', '1234554321', 'robertDJ@yahoo.com', '1ronm@n', '@robertdowneyjr', 'https://i.imgur.com/oVdHbN1.jpeg', 'I''m your ideal roommate, enough about me. How about you? I''m looking for a roommate with a good head on their shoulders, doesn''t mind the occassional impromptu party and respects each other''s privacy', 'Wonderland', TRUE), 
 
-('Jim', 'Carrey', 'Jim_Carrey', '1235678921', 'jim_carrey@wise.com', 'oneForall', '@jim_carrey', 'https://i.imgur.com/7KICHYq.jpeg', 'Looking for a roommate temporarily while I visit LA, need someone who can watch over my dogs while I am away. Love to paint', 'Los Angeles', TRUE), 
+('Jim', 'Carrey', 'Jim_Carrey', '1235678921', 'jim_carrey@wise.com', 'oneForall', '@JimCarreyOnline', 'https://i.imgur.com/7KICHYq.jpeg', 'Looking for a roommate temporarily while I visit LA, need someone who can watch over my dogs while I am away. Love to paint', 'Los Angeles', TRUE), 
 
-('Jeff', 'Goldblum', 'Jeff_Goldblum', '9098621232', 'jeff_goldblum@jurrassic.com', 'Dinosaurs', '@jeff_goldblum', 'https://i.imgur.com/XydNcOT.jpeg', 'My house got destroyed by a T-Rex during Jurrassic World - dominion. Looking for a roommate who can handle being chased by dinosaurs and safeguard my belongings while I save the world', 'San Francisco', TRUE),
+('Jeff', 'Goldblum', 'Jeff_Goldblum', '9098621232', 'jeff_goldblum@jurrassic.com', 'Dinosaurs', '@jeffgoldblum', 'https://i.imgur.com/XydNcOT.jpeg', 'My house got destroyed by a T-Rex. Looking for a roommate who can handle my humor and safeguard my belongings while I save the world', 'San Francisco', TRUE),
 
-('Blake', 'Lively', 'blake_lively', '3478901022', 'blake_lively@reynolds.com', 'Dinosaurs', '@blake_lively', 'https://i.imgur.com/yQVTifC.jpeg', 'Looking to find a temporary roommate with kids who can hang out with my kids while I stay in chicago.', 'Chicago', FALSE),
+('Blake', 'Lively', 'blake_lively', '3478901022', 'blake_lively@reynolds.com', 'Dinosaurs', '@blakelively', 'https://i.pinimg.com/originals/d8/71/d7/d871d7433730006460afbb9fbbd59363.jpg', 'Looking to find a temporary roommate who loves to bake like I do.', 'Chicago', FALSE),
 
-('Nina', 'Dobrev', 'nina_dobrev', '4564443877', 'nina_dobrev@vamp.com', 'ninaClassic', '@ninaDobrev', 'https://i.imgur.com/WyMWFoK.jpeg', 'Looking for a roommate who is participating in the olympics', 'British Columbia', FALSE),
+('Nina', 'Dobrev', 'nina_dobrev', '4564443877', 'nina_dobrev@vamp.com', 'ninaClassic', '@ninadobrev', 'https://i.pinimg.com/736x/9a/23/54/9a2354c01096a9fc929afe6a090d61cf.jpg', 'Looking for a roommate who loves outdoor activities and does not mind vampire parties', 'British Columbia', FALSE),
 
-('Tom', 'Cruise', 'Need4Speed', '5648651182', 'mr.cruise.is.awesome@gmail.com', 'need2speed', 'Twitter: @tomcruise', 'https://i.imgur.com/TUSe75T.jpeg', 'I am an active thrill seeker looking for roommates who feel the need to continue to push themselves everyday in every field towards excellence. If this is you please reach out to me.', 'Florida', TRUE),
+('Tom', 'Cruise', 'Need4Speed', '5648651182', 'mr.cruise.is.awesome@gmail.com', 'need2speed', 'Twitter: @TomCruise', 'https://i.imgur.com/TUSe75T.jpeg', 'I am an active thrill seeker looking for roommates who feel the need to continue to push themselves everyday in every field towards excellence. If this is you please reach out to me.', 'Florida', TRUE),
 
-('Emma', 'Watson', 'Weasley_Fan504', '6595469987', 'hermoine.granger@gmail.com', 'occulus_repairo', 'Twitter: @emmawatson', 'https://i.imgur.com/AJQrMa3.jpeg', 'My last roommate kind of got turned into a toad. Midterms were hell. Looking for a roommate who doesn''t mind my cat.', 'Toronto', FALSE),
+('Emma', 'Watson', 'Weasley_Fan504', '6595469987', 'hermoine.granger@gmail.com', 'occulus_repairo', 'Twitter: @EmmaWatson', 'https://i.pinimg.com/originals/28/5c/a9/285ca965ec6218d5f5a6729b94d87d8f.jpg', 'My last roommate kind of got turned into a toad. Midterms were hell. Looking for a roommate who doesn''t mind my cat.', 'Toronto', FALSE),
 
-('Jensen', 'Ackles', 'Dean_Winchester_001', '6595469987', 'dean.winchester@gmail.com', 'wakeupsammie', 'Twitter: @jensenackles', 'https://i.imgur.com/vwOE7Ax.jpeg', 'Going on a family trip with my brother, looking for someone to hold the fort down. Just don''t touch my stuff.', 'Vancouver', TRUE),
+('Jensen', 'Ackles', 'Dean_Winchester_001', '6595469987', 'dean.winchester@gmail.com', 'wakeupsammie', 'Twitter: @JensenAckles', 'https://i.imgur.com/vwOE7Ax.jpeg', 'Going on a family trip with my brother, looking for someone to hold the fort down. Just don''t touch my stuff.', 'Vancouver', TRUE),
 
-('Olivia', 'Wilde', 'tron_girl_1337', '2265981337', 'ai.tron.girl@gmail.com', 'cowboys&aliens', 'Twitter: @oliviawilde', 'https://i.imgur.com/5UfwI.jpeg', 'Looking for roomies who love 1980s arcade games. I already have a place and just need someone to rent the other room!', 'Ottawa', FALSE),
+('Olivia', 'Wilde', 'tron_girl_1337', '2265981337', 'ai.tron.girl@gmail.com', 'cowboys&aliens', 'Twitter: @oliviawilde', 'https://pbs.twimg.com/media/Bv-OS0uIYAAk974.jpg', 'Looking for roomies who love 1980s arcade games. I already have a place and just need someone to rent the other room!', 'Ottawa', FALSE),
 
-('Lighthouse', 'Labber', 'lighthouse_labber2022', '5466651227', 'lighthouse_labber2022@lighthouselabs.com', 'labber', 'Twitter: @lighthouselabber2022', 'https://www.zachlevy.me/static/media/lighthouse_labs_logo_1024x512.c4b852ba.png','I''m a lighthouse labber', 'Toronto', NULL)
+('Lighthouse', 'Labber', 'lighthouse_labber2022', '5466651227', 'lighthouse_labber2022@lighthouselabs.com', 'labber', 'Twitter: @lighthouselabber2022', 'https://applywave.com/wp-content/uploads/2020/09/lhllogo-600x600.png','I''m a lighthouse labber who loves code!', 'Toronto', NULL)
